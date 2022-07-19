3ab8e926f651c136b857f55b6bed6c611c84e679
John U. Amah <84844009+foresightJ@users.noreply.github.com>
2022-07-19 14:11:47 -0400

    created react app and set up the basic skeleton for the express backend app, set up proxy to handle ajax calls during develeopment

---
+++ server.js
@@ -0,0 +1,35 @@
+// requiring all needed modules
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+
+// creating the express app
+const app = express();
+
+/**  mounting the morgan & JSON middleware to 
+log dev results and process JSON data respectively */
+app.use(logger('dev'));
+app.use(express.json());
+
+/** mounting and configuring both serve-favicon & static middleware
+to serve from production 'build' folder */
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+/** configuring the backend to use port 3001 to avoid collision with 
+with react's dev server on port 3000 */
+const port = process.env.PORT || 3001;
+
+/** configuring the Express app to liston for http request */
+app.listen(port, function () {
+	console.log(`Express app running on port ${port}`);
+});
+
+// All API routes here, before the "catch all" route goes here
+
+/** Defining a route to "catch all" non-AJAX request and serve up the index.html page 
+Note: the * is necessary to return the index.html on all non-AJAX requests */
+app.get('/*', function (req, res) {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
